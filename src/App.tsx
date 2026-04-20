5809560a288d9afae32b3a56b02ffb0e29395870
IndoViet Hub <info@indoviet-hub.com>
2026-04-20 20:26:41 +0200

    Ajouter ScrollToTop pour remonter en haut de page a chaque navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,6 +13,7 @@ import FAQ from './pages/FAQ';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Header />
         <Routes>
